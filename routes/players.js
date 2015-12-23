15d7ede7d22ca2771482e5753b51723083466b3a
Ian Behrmann <ianjbehrmann@gmail.com>
2015-12-23 16:23:05 -0700

    years route created

--- routes/players.js
+++ routes/players.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../db/models/index');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('postgres://localhost:5432/nbaplayers-dev', {})
+
 
 router.get('/', function (req, res, next) {
   models.players.findAll({}).then(function (players) {
     res.json(players);
+  })
+})
+
+router.get('/years', function (req, res, next) {
+  sequelize.query('SELECT DISTINCT "draftYear" FROM players order by "draftYear"').then(function (years) {
+    res.json(years[0].map(function(record){
+      return record.draftYear;
+    }));
   })
 })
 
